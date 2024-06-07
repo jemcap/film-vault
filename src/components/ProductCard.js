b777031659ae3ab21e9ac348bbae540ba004aa5f
JCapito <joshuaecapito22@gmail.com>
2024-06-07 12:57:37 +0100

    feature: Implemented Product Card component

---
+++ src/components/ProductCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card, Button, Form, Col, Row } from "react-bootstrap";
+
+const ProductCard = ({ product }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{product.text}</Card.Title>
+        <Card.Text>{product.price}</Card.Text>
+        <Button variant="primary">Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductCard;
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import { productsArr } from "../context/productStore";
+import ProductCard from "../components/ProductCard";
 const Store = () => {
   return (
     <>
-      <h1 align="center" className="mb-2">
+      <h1 align="center" className="p-3">
         Used Tech Products
       </h1>
       <Row xs={1} md={3} className="g-4">
-        {productsArr.map((product) => (
-          <Col align="center">
+        {productsArr.map((product, index) => (
+          <Col align="center" key={index}>
             {/* <img src={product.image} width={300} /> */}
-            <h1>{product.text}</h1>
-            <p>{product.price}</p>
+            <ProductCard product={product} />
           </Col>
         ))}
       </Row>
