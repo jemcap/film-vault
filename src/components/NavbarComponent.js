2a44c8869f4afb170a6045cdc9b1f7521e6c710e
JCapito <joshuaecapito22@gmail.com>
2024-06-07 10:59:52 +0100

    feature: Added Navbar component

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../assets/Film-Vault_transparent.png";
+import { Navbar, Container, Button, Modal } from "react-bootstrap";
+
+const NavbarComponent = () => {
+  return (
+    <Navbar expand="sm">
+      <Navbar.Brand href="/">
+        <img src={logo} width={150} />
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Button>Cart 0 Items</Button>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
