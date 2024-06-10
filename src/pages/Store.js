ebb3d0f362551d49f73f47b75a9e4c90adbcdf46
JCapito <joshuaecapito22@gmail.com>
2024-06-10 11:22:02 +0100

    major feature: Implemented filtering by product category and amendments to product list

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { productsArr } from "../context/productStore";
 import ProductCard from "../components/ProductCard";
+import FilterButtons from "../components/FilterButtons";
 const Store = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const products = productsArr;
+  const productCategories = [
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : products.filter((product) => product.category === selectedCategory);
+
   return (
     <>
+      <FilterButtons
+        categories={productCategories}
+        selectedCategory={selectedCategory}
+        onCategorySelect={setSelectedCategory}
+      />
       <h3 align="center" className="p-3">
-        All
+        {selectedCategory === "All" ? "All" : selectedCategory}
       </h3>
       <Row xs={1} md={3} className="g-4">
-        {productsArr.map((product, index) => (
+        {filteredProducts.map((product, index) => (
           <Col align="center" key={index}>
             <ProductCard product={product} />
           </Col>
